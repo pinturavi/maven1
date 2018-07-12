33b8e2d5600795bfe028438b5a76fbaf81d4dfce
pintu ravi <pintu541796@gmail.com>
2018-07-12 08:11:05 +0530

    changed print

--- src/main/java/com/pintu/maven1/App.java
+++ src/main/java/com/pintu/maven1/App.java
@@ -2,6 +2,6 @@ package com.pintu.maven1;
 
 public class App {
 	public static void main(String args[]){
-		System.out.println("Hello PIntu Ravi");
+		System.out.println("Hello PIntu Ravi Gowda");
 	}
 }
